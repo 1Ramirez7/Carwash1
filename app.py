011d501668e8fa0ce0a5d44ddd636a6f9ca68925
ERamirez <ram20004@byui.edu>
2024-06-22 19:14:35 -0600

    app update

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = 'eduardo.er.ramirez@gmail.com'  # Replace with your Gmail address
 app.config['MAIL_PASSWORD'] = 'jqku emne ftsh tadk'  # Replace with your app-specific password
 app.config['MAIL_DEFAULT_SENDER'] = 'eduardo.er.ramirez@gmail.com'  # Replace with your Gmail address
